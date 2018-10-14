603b7ebd010ac8d3e85530cf267edc43fdda53ae
kjano10 <kuba.jano3@gmail.com>
2018-10-14 16:15:39 +0200

    ex4 done

---
+++ lab1/zad4/prog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <float.h>
+
+int main(void){
+    printf("\n-----SINGLE PRECISION-----\n");
+    float myVal = 1.0;
+    float myEps = 1.0;
+
+    while(myVal+(float)(myEps *0.5)!=myVal)
+        myEps*=0.5;
+    
+    printf("My epsilon: %g\n", myEps);
+    printf("Definied in float.h: %g\n", FLT_EPSILON);
+
+    printf("\n-----DOUBLE PRECISION-----\n");
+
+    double myValD = 1.0;
+    float myEpsD = 1.0;
+
+    while(myValD + (double)(myEpsD*0.5)!=myValD)
+        myEpsD*=0.5;
+
+    printf("My epsilon: %g\n", myEpsD);
+    printf("Definied in float.h: %g\n", DBL_EPSILON);
+}
